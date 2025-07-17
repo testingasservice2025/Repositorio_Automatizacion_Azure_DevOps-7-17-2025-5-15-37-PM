b14120e303ef776124a05a385163c3d1ed21e6f1
testingasservice2025 <testingasservice@gmail.com>
2025-07-17 11:24:50 -0600

    agregando src/test/java/pages/tc30Page.java

---
+++ src/test/java/pages/tc30Page.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Tc30Page {
+    private WebDriver driver;
+
+    public Tc30Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void consultarClienteEnCBSYActicenter() {
+        // Implementación para consultar cliente en CBS y Acticenter
+    }
+
+    public void mostrarFuentesDeInformacion() {
+        // Implementación para mostrar múltiples fuentes de información
+    }
+
+    public void validarConsistenciaInfo() {
+        // Implementación para validar la consistencia entre CBS y Acticenter
+    }
+
+    public void confirmarIntegridadVisual() {
+        // Implementación para confirmar la integridad visualizando datos sincronizados
+    }
+
+    public void verificarDatosIntegrales() {
+        // Implementación para verificar que los datos sean integrales y sin discrepancias
+    }
+}
