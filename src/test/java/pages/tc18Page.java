a38070071e4e14b3d2918bb855277832135cbc09
testingasservice2025 <testingasservice@gmail.com>
2025-07-17 11:24:42 -0600

    agregando src/test/java/pages/tc18Page.java

---
+++ src/test/java/pages/tc18Page.java
@@ -0,0 +1,31 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc18Page {
+    WebDriver driver;
+
+    public tc18Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void invokeShellScripts() {
+        // Código Java para invocar los scripts shell
+    }
+
+    public void verifyDataTransformationAndLoad() {
+        // Código Java para verificar la transformación y carga de datos en la base de datos
+    }
+
+    public void verifyDataSentToDatabase() {
+        // Código Java para comprobar que la información se envió correctamente a la base de datos
+    }
+
+    public void notifySuccess() {
+        // Código Java para notificar el éxito del procesamiento ETL
+    }
+
+    public void verifyConfirmationMessageDisplayed() {
+        // Código Java para verificar que se muestre el mensaje confirmatorio en la interfaz
+    }
+}
