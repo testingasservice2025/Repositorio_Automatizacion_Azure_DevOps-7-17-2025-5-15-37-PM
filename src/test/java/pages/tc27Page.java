19a81872964ea6c5e722d8975213f139924cae81
testingasservice2025 <testingasservice@gmail.com>
2025-07-17 11:24:48 -0600

    agregando src/test/java/pages/tc27Page.java

---
+++ src/test/java/pages/tc27Page.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc27Page {
+
+    WebDriver driver;
+
+    // Constructor
+    public tc27Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void consultarCliente() {
+        // Lógica para realizar la consulta del cliente asesorado
+    }
+
+    public void iniciarCargaDeInformacion() {
+        // Lógica para iniciar la carga de información
+    }
+
+    public void consolidarDatos() {
+        // Lógica para consolidar datos de cliente, contrato e inversiones
+    }
+
+    public void verificarOrganizacionIntegral() {
+        // Lógica para verificar que la información esté organizada de forma integral
+    }
+
+    public void revisarDashboard() {
+        // Lógica para que el operador revise el dashboard
+    }
+
+    public void verificarIntegridad() {
+        // Lógica para verificar que el sistema muestre información sin inconsistencias
+    }
+}
